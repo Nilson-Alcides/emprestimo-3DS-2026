8cc1d6d9fce76ee617594f8b334e9fa7a25443bc
Nilson-Alcides <nilson_alcides@hotmail.com>
2026-04-29 08:41:23 -0300

    Add projeto

---
+++ Emprestimo_3DS_2026/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Emprestimo_3DS_2026.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
